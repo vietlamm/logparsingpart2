4830a9f19a146866da5aff60fbeae42af051e951
vietlamm <66132602+vietlamm@users.noreply.github.com>
2022-02-23 14:42:08 -0600

    Update start.py

--- start.py
+++ start.py
@@ -10,7 +10,18 @@ local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
 
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE, lambda x,y,z: print('.', end='', flush=True) if x % 100 == 0 else False)
 
-j=0
+jan=0
+feb=0
+march=0
+april=0
+may=0
+june=0
+july=0
+august=0
+sep=0
+oct=0
+nov=0
+dec=0
 
 #creating a list for the log file and running it through a loop
 #need to create another loop so that it reads the file and see whats month it is so it can split the file based on what month it is
@@ -18,6 +29,5 @@ with open("local_copy.log", "r") as file:
   for l in file:
     d=l.split()
       if re.search ('(/Jan/)', l):
-      j+=1
-      
+      jan+=1
       print(d)
